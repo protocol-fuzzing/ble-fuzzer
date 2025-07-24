8c7c2be8cdaf0af09e1aab13f02c94cb7e5936fe
Kostis Sagonas <kostis@it.uu.se>
2025-07-24 08:48:14 +0200

    Explicitly specify shellcheck directives to ignore a (non-available) source file

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script sets up the python environment if it doesn't exist yet.
 # It should be sourced (i.e. called with "source scripts/setup_venv.sh")
 # so that the python environment will be available in the calling shell.
 
 if [ -d "venv" ]; then
+    # shellcheck source=/dev/null
     source venv/bin/activate
 else
     # Create a venv and activate it
     python3 -m venv venv
+    # shellcheck source=/dev/null
     source venv/bin/activate
     # Install dependencies from PyPi
     pip3 install -r requirements.txt
