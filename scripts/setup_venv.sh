7b336b257461ea4b470e4e97cd6192523e69713d
Kostis Sagonas <kostis@it.uu.se>
2025-07-25 16:09:28 +0200

    Add shellcheck action (#5)
    * Add shellcheck action
    
    * Fix some shellcheck issues
    
    * Specify an option to invoke shellcheck with
    
    * Fix typo
    
    * Explicitly specify shellcheck directives to ignore a (non-available) source file
    
    * Fix order of read options (-p should be with the prompt)

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -1,22 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script sets up the python environment if it doesn't exist yet.
 # It should be sourced (i.e. called with "source scripts/setup_venv.sh")
 # so that the python environment will be available in the calling shell.
 
 if [ -d "venv" ]; then
+    # shellcheck source=/dev/null
     source venv/bin/activate
 else
     # Create a venv and activate it
     python3 -m venv venv
+    # shellcheck source=/dev/null
     source venv/bin/activate
     # Install dependencies from PyPi
     pip3 install -r requirements.txt
     # Install the BLESMPServer
     git clone https://github.com/apferscher/ble-learning.git
-    cd ble-learning
+    cd ble-learning || exit
     git checkout febd774109c41a6635659b8847cc766e667841dd
-    cd libs/smp_server
+    cd libs/smp_server || exit
     python3 setup.py install
     cd ../../..
     rm -rf ble-learning
