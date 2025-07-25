583271c2f6691f62826216c0e0facbd0547591fe
Kostis Sagonas <kostis@it.uu.se>
2025-07-25 14:58:20 +0200

    Fix order of read options (-p should be with the prompt)

--- scripts/reset_manually.sh
+++ scripts/reset_manually.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 notify-send -t 3600000 -a ProtocolStateFuzzer -h string:x-canonical-private-synchronous:my-notification ProtocolStateFuzzer 'Device reset needed'
-read -p -r 'PLEASE RESET THE DEVICE AND THEN PRESS ENTER TO CONTINUE...'
+read -r -p 'PLEASE RESET THE DEVICE AND THEN PRESS ENTER TO CONTINUE...'
 notify-send -t 1 -a ProtocolStateFuzzer -h string:x-canonical-private-synchronous:my-notification ProtocolStateFuzzer 'Continuing'
 
